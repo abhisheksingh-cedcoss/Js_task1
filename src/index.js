038490508b30c204c454d39b22f8b41ca211a071
abhisheksingh-cedcoss <abhisheksingh2022jan@cedcommerce.com>
2022-02-10 17:23:05 +0530

    all parts done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+function myfunction1(){
+
+    var l = document.getElementById("length_value").value;
+    var b = document.getElementById("length_value").value;
+    
+    x= parseInt(l);
+    y= parseInt(b);
+
+
+document.getElementById("result1").innerHTML= "Area is "+ x*y + " mtr";
+document.getElementById("result2").innerHTML= "Perimeter is "+ 2*(x+y) + " mtr";
+
+}
+
 
